eecb8244bed1637f7c99a5f58027fb506624acca
Harineko0 <harineko0927@gmail.com>
2025-08-10 19:41:36 +0900

    Add availableSlot and availableMaterial restirctions to MaterialEditor.java

--- src/main/java/jp/mincra/mincramagics/gui/impl/MaterialEditor.java
+++ src/main/java/jp/mincra/mincramagics/gui/impl/MaterialEditor.java
@@ -8,6 +8,7 @@ import jp.mincra.mincramagics.MincraMagics;
 import jp.mincra.mincramagics.constant.Color;
 import jp.mincra.mincramagics.gui.InventoryGUI;
 import jp.mincra.mincramagics.nbtobject.MagicStaffNBT;
+import jp.mincra.mincramagics.MaterialSlot;
 import jp.mincra.mincramagics.skill.MaterialManager;
 import jp.mincra.titleupdater.InventoryUpdate;
 import me.clip.placeholderapi.PlaceholderAPI;
@@ -25,25 +26,34 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class MaterialEditor extends InventoryGUI {
-    private final static List<String> slotIdList = Arrays.asList("left", "right", "swap", "drop");
-    // region oraxen glyph
+    // Oraxen Glyphs
     private static final String NEG_128 = "%oraxen_neg_shift_128%";
     private static final String NEG_64 = "%oraxen_neg_shift_64%";
     private static final String NEG_8 = "%oraxen_neg_shift_8%";
     private static final String GUI_ACTIVATED = "%oraxen_gui_material_editor_activated%";
     private static final String GUI_INACTIVATED = "%oraxen_gui_material_editor_inactivated%";
-    // endregion
 
-    private final Inventory inv;
+    // Slot Indexes
+    private static final int FIRST_SLOT_INDEX = 0;
+    private static final int ARTIFACT_SLOT_INDEX = 10;
+    private static final int FIRST_MATERIAL_SLOT_INDEX = 12;
+    private static final int LAST_MATERIAL_SLOT_INDEX = 16;
+    private static final int MATERIAL_EDITOR_SIZE = 27;
+
     private final MaterialManager materialManager;
+    private final Logger logger;
+    private final Inventory inv;
     private final JavaPlugin mincramagics;
     private Player player;
     private final ItemStack invisibleItem;
+    private final ItemStack unavailableSlotItem;
 
     private final static String activeTitle =
             Color.COLOR_WHITE +
@@ -64,9 +74,24 @@ public class MaterialEditor extends InventoryGUI {
                 inv.setItem(i, invisibleItem);
             }
         }
+        unavailableSlotItem = OraxenItems.getItemById("unavailable_slot").build();
         materialManager = MincraMagics.getMaterialManager();
         mincramagics = MincraMagics.getInstance();
+        logger = MincraMagics.getPluginLogger();
     }
+
+    @Override
+    protected void open(Player player) {
+        player.openInventory(inv);
+        this.player = player;
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inv;
+    }
+
+    // Event Listeners
 
     @EventHandler
     private void onClick(InventoryClickEvent event) {
@@ -86,16 +111,15 @@ public class MaterialEditor extends InventoryGUI {
                 // プレイヤーインベントリ -> Material作業台
                 int topLeftSlot = getTopLeftEmptySlot();
                 if (isMaterialEditorSlot(topLeftSlot)) {
-                    boolean success = onPlace(topLeftSlot, currentItem);
-                    event.setCancelled(!success);
+                    event.setCancelled(!handlePlaceItem(topLeftSlot, currentItem));
                 }
             } else {
                 //  Material作業台 -> プレイヤーインベントリ
-                onPickup(slot);
+                event.setCancelled(!handlePickupItem(slot));
             }
         } else {
             // プレイヤーのインベントリのとき
-            if (slot > 26) {
+            if (slot >= MATERIAL_EDITOR_SIZE) {
                 return;
             }
 
@@ -109,13 +133,13 @@ public class MaterialEditor extends InventoryGUI {
             ItemStack cursorItem = event.getCursor();
 
             if (action == InventoryAction.PLACE_ALL || action == InventoryAction.PLACE_ONE) {
-                event.setCancelled(!onPlace(slot, cursorItem));
+                event.setCancelled(!handlePlaceItem(slot, cursorItem));
             } else if (action == InventoryAction.PICKUP_ALL) {
-                onPickup(slot);
+                event.setCancelled(!handlePickupItem(slot));
             } else if (action == InventoryAction.SWAP_WITH_CURSOR && !isMagicStaffSlot(slot)) {
                 // マテリアルの入れ替えしかできない(magicstuff入れ替えは実装がめんどくさい)
-                onPickup(slot);
-                event.setCancelled(!onPlace(slot, cursorItem));
+                event.setCancelled(!handlePickupItem(slot));
+                event.setCancelled(!handlePlaceItem(slot, cursorItem));
             } else {
                 event.setCancelled(true);
             }
@@ -139,7 +163,7 @@ public class MaterialEditor extends InventoryGUI {
         // MaterialEditorのインベントリかどうか
         if (!event.getInventory().equals(getInventory())) return;
 
-        ItemStack magicStuff = getMagicStaff();
+        ItemStack magicStuff = getMagicStaffItem();
         // 魔法武器がまだスロットに入ってたら
         if (magicStuff != null) {
             if (isFullInventory(player)) {
@@ -152,68 +176,86 @@ public class MaterialEditor extends InventoryGUI {
         }
     }
 
+    // Event Handlers
+
     /**
-     *
+     * アイテムを置いたときの処理
      * @param slot 置かれたスロット
      * @param placedItem 置かれたアイテム
      * @return 適切なアイテムが置かれたらtrue
      */
-    private boolean onPlace(int slot, ItemStack placedItem) {
+    private boolean handlePlaceItem(int slot, ItemStack placedItem) {
         if (isMagicStaffSlot(slot)) {
             return setArtifact(placedItem, slot);
         } else {
             // マテリアルのとき
+            if (!isAvailableMaterialSlot(slot)) {
+                return false;
+            }
             return setMaterial(placedItem, slot);
         }
     }
 
-    private void onPickup(int slot) {
+    /**
+     * アイテムを取ったときの処理
+     * @param slot 取られたスロット
+     */
+    private boolean handlePickupItem(int slot) {
         if (isMagicStaffSlot(slot)) {
-            // マテリアルを削除
-            for (int i = 12; i < 16; i++) {
-                inv.setItem(i, null);
-            }
-            changeTitle(inactiveTitle);
+            return unsetArtifact(slot);
         } else {
-            // マテリアルを取ったとき
-            setMaterial(null, slot);
+            if (!isAvailableMaterialSlot(slot)) {
+                return false; // 利用可能なマテリアルスロットでない場合は何もしない
+            }
+            return unsetMaterial(slot);
         }
     }
 
-    @Override
-    protected void open(Player player) {
-        player.openInventory(inv);
-        this.player = player;
-    }
+    // Slot Utilities
+    private boolean isAvailableMaterialSlot(int slot) {
+        if (slot < FIRST_MATERIAL_SLOT_INDEX || slot >= LAST_MATERIAL_SLOT_INDEX) {
+            return true; // マテリアルスロットの範囲外
+        }
 
-    @Override
-    public Inventory getInventory() {
-        return inv;
+        final ItemStack artifact = getMagicStaffItem();
+        final MagicStaffNBT artifactNBT = MagicStaffNBT.getMincraNBT(artifact);
+        if (artifactNBT == null) {
+            return true; // 魔法武器がない場合は未使用スロット
+        }
+        final MaterialSlot materialSlot = MaterialSlot.fromIndex(slot - FIRST_MATERIAL_SLOT_INDEX);
+        if (materialSlot == null) {
+            logger.warning("Invalid MaterialSlot index: " + (slot - FIRST_MATERIAL_SLOT_INDEX));
+            return true;
+        }
+        return artifactNBT.isAvailableSlot(materialSlot);
     }
 
     private boolean isAvailableSlot(int slot) {
-        return slot > 26 || slot == 10 || (slot > 11 && slot < 16);
+        return slot == ARTIFACT_SLOT_INDEX || (FIRST_MATERIAL_SLOT_INDEX <= slot && slot < LAST_MATERIAL_SLOT_INDEX) || MATERIAL_EDITOR_SIZE <= slot;
     }
 
     private boolean isMaterialEditorSlot(int slot) {
-        return slot < 27;
+        return slot < MATERIAL_EDITOR_SIZE;
     }
 
     private boolean isTransparentSlot(int slot) {
-        return (slot > 0 && slot < 10) || slot == 11 || (slot > 15 && slot < 27);
+        return (FIRST_SLOT_INDEX < slot && slot < ARTIFACT_SLOT_INDEX) || (ARTIFACT_SLOT_INDEX < slot && slot < FIRST_MATERIAL_SLOT_INDEX) || (LAST_MATERIAL_SLOT_INDEX <= slot && slot < MATERIAL_EDITOR_SIZE);
     }
 
-    private ItemStack getMagicStaff() {
-        return inv.getItem(10);
+    @Nullable
+    private ItemStack getMagicStaffItem() {
+        return inv.getItem(ARTIFACT_SLOT_INDEX);
     }
 
     private void setMagicStaff(ItemStack item) {
-        inv.setItem(10, item);
+        inv.setItem(ARTIFACT_SLOT_INDEX, item);
     }
 
     private boolean isMagicStaffSlot(int slot) {
-        return slot == 10;
+        return slot == ARTIFACT_SLOT_INDEX;
     }
+
+    // Item modification methods
 
     private boolean setArtifact(ItemStack artifactItem, int slot) {
         if (artifactItem == null) {
@@ -227,16 +269,44 @@ public class MaterialEditor extends InventoryGUI {
         Map<String, String> materialMap = magicStaffNBT.getMaterialMap();
 
         for (int i = 0; i < 4; i++) {
-            String slotId = slotIdList.get(i);
+            MaterialSlot mSlot = MaterialSlot.fromIndex(i);
+            if (mSlot == null) {
+                logger.warning("Invalid MaterialSlot index: " + i);
+                continue;
+            }
+            String slotId = mSlot.getSlot();
             if (materialMap.containsKey(slotId)) {
                 String materialId = materialMap.get(slotId);
                 if (OraxenItems.exists(materialId)) {
                     ItemStack material = OraxenItems.getItemById(materialId).build();
-                    inv.setItem(12 + i, material);
+                    inv.setItem(FIRST_MATERIAL_SLOT_INDEX + i, material);
                 }
             }
         }
+
+        // 利用可能でないスロットに barrier を置く
+        final MaterialSlot[] allSlots = MaterialSlot.values();
+        final List<MaterialSlot> availableSlots = magicStaffNBT.availableSlots();
+        final List<MaterialSlot> unavailableSlots = Arrays.stream(allSlots)
+                .filter(materialSlot -> !availableSlots.contains(materialSlot))
+                .toList();
+        for (MaterialSlot mSlot : unavailableSlots) {
+            int index = MaterialSlot.indexOf(mSlot);
+            if (index != -1) {
+                inv.setItem(FIRST_MATERIAL_SLOT_INDEX + index, unavailableSlotItem);
+            }
+        }
+
         changeTitle(activeTitle);
+        return true;
+    }
+
+    private boolean unsetArtifact(int slot) {
+        // マテリアルを削除
+        for (int i = FIRST_MATERIAL_SLOT_INDEX; i < LAST_MATERIAL_SLOT_INDEX; i++) {
+            inv.setItem(i, null);
+        }
+        changeTitle(inactiveTitle);
         return true;
     }
 
@@ -247,7 +317,7 @@ public class MaterialEditor extends InventoryGUI {
     private boolean setMaterial(ItemStack materialItem, int slot) {
         // マテリアルを取ったとき
         // 魔法武器が置いてなかったらキャンセル
-        ItemStack magicStuff = getMagicStaff();
+        ItemStack magicStuff = getMagicStaffItem();
         if (magicStuff == null) return false;
 
         // 魔法武器のNBT入手
@@ -258,13 +328,22 @@ public class MaterialEditor extends InventoryGUI {
             String oraxenMaterialItemId = OraxenItems.getIdByItem(materialItem);
             if (!OraxenItems.exists(materialItem)) return false;
             if (!materialManager.isRegistered(oraxenMaterialItemId)) return false;
-            magicStaffNBT.setMaterial(slotIdList.get(slot - 12), oraxenMaterialItemId);
+            if (!magicStaffNBT.isAvailableMaterial(oraxenMaterialItemId)) return false;
+
+            magicStaffNBT.setMaterial(MaterialSlot.fromIndexAsString(slot - FIRST_MATERIAL_SLOT_INDEX), oraxenMaterialItemId);
         } else {
-            magicStaffNBT.removeMaterial(slotIdList.get(slot - 12));
+            magicStaffNBT.removeMaterial(MaterialSlot.fromIndexAsString(slot - FIRST_MATERIAL_SLOT_INDEX));
         }
 
         ItemStack newItem = magicStaffNBT.setNBTTag(new ItemBuilder(magicStuff)).build();
         setMagicStaff(newItem);
+
+        return true;
+    }
+
+    private boolean unsetMaterial(int slot) {
+        // マテリアルを削除
+        setMaterial(null, slot);
 
         return true;
     }
