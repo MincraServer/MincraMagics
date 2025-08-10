822bdfa00a9572efa164ddb8ec2a99a213777123
Harineko0 <harineko0927@gmail.com>
2025-08-10 15:22:42 +0900

    Add Javadoc

--- src/main/java/jp/mincra/mincramagics/oraxen/mechanic/magicstaff/TriggerType.java
+++ src/main/java/jp/mincra/mincramagics/oraxen/mechanic/magicstaff/TriggerType.java
@@ -1,5 +1,10 @@
 package jp.mincra.mincramagics.oraxen.mechanic.magicstaff;
 
+/**
+ * TriggerType when player uses the material actively.
+ * Note that this class is different from MaterialSlot, as the former is used for skill activation, and the latter is used for slots.
+ * For instance, the latter may contain a passive slot, but the former does not.
+ */
 public enum TriggerType {
     LEFT, RIGHT, SWAP, DROP
 }
