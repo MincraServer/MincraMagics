ec462164adac97ba354d87b7e74c9583c348d4ba
Harineko0 <harineko0927@gmail.com>
2025-08-10 19:38:12 +0900

    Add NamespacedKeys.java, PDCUtils.java for String array container

---
+++ src/main/java/jp/mincra/mincramagics/nbtobject/NamespacedKeys.java
@@ -0,0 +1,22 @@
+package jp.mincra.mincramagics.nbtobject;
+
+import jp.mincra.mincramagics.MincraMagics;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NamespacedKeys {
+    private static final JavaPlugin mincra = MincraMagics.getInstance();
+
+    public static final NamespacedKey MINCRA_MAGICS_KEY = new NamespacedKey(mincra, "MincraMagics");
+
+    // Material
+    public static final NamespacedKey MATERIALS_KEY = new NamespacedKey(mincra, "Materials");
+    public static final NamespacedKey SLOT_KEY = new NamespacedKey(mincra, "Slot");
+    public static final NamespacedKey ID_KEY = new NamespacedKey(mincra, "Id");
+    public static final NamespacedKey LORE_KEY = new NamespacedKey(mincra, "LORE");
+    public static final NamespacedKey AVAILABLE_SLOTS_KEY = new NamespacedKey(mincra, "AvailableSlots");
+    public static final NamespacedKey AVAILABLE_MATERIALS_KEY = new NamespacedKey(mincra, "AvailableMaterials");
+
+    // common
+    public static final NamespacedKey VALUE_KEY = new NamespacedKey(mincra, "value");
+}
