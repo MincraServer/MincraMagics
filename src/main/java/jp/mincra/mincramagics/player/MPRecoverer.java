9d66f0202b459cab74f24ccec12ac6321859f823
Harineko0 <harineko0927@gmail.com>
2025-08-13 15:07:03 +0900

    Add tmp code to MPRecoverer.java

--- src/main/java/jp/mincra/mincramagics/player/MPRecoverer.java
+++ src/main/java/jp/mincra/mincramagics/player/MPRecoverer.java
@@ -15,12 +15,13 @@ import java.util.UUID;
 
 public class MPRecoverer implements Listener {
     private final PlayerManager playerManager;
-    private final Map<UUID, MincraPlayer> sittingPlayers;
+//    private final Map<UUID, MincraPlayer> inCombatPlayers = new HashMap<>();
+    private final Map<UUID, MincraPlayer> sittingPlayers = new HashMap<>();
 
     public MPRecoverer(MincraMagics instance, PlayerManager playerManager) {
         this.playerManager = playerManager;
-        sittingPlayers = new HashMap<>();
 
+        // sitting regeneration
         new BKTween(instance)
                 .execute(v -> {
                     for (MincraPlayer player : sittingPlayers.values()) {
@@ -28,8 +29,22 @@ public class MPRecoverer implements Listener {
                     }
                     return true;
                 })
-                .repeat(TickTime.SECOND, 1, 0, -1)
+                .repeat(TickTime.SECOND, 3, 0, -1)
                 .run();
+
+        // general MP regeneration
+        // TODO: MP自然回復系のスキルを作る時に流用する
+//        new BKTween(instance)
+//                .execute(v -> {
+//                    for (MincraPlayer player : playerManager.getPlayers()) {
+//                        if (!inCombatPlayers.containsKey(player.getPlayer().getUniqueId())) {
+//                            player.addMp(1, false);
+//                        }
+//                    }
+//                    return true;
+//                })
+//                .repeat(TickTime.SECOND, 20, 0, -1)
+//                .run();
     }
 
     @EventHandler
@@ -46,4 +61,25 @@ public class MPRecoverer implements Listener {
             sittingPlayers.remove(player.getUniqueId());
         }
     }
+
+//    @EventHandler
+//    private void onPlayerDamage(EntityDamageByEntityEvent event) {
+//        if (event.getEntity() instanceof Player player) {
+//            UUID uuid = player.getUniqueId();
+//            MincraPlayer mincraPlayer = playerManager.getPlayer(uuid);
+//            if (mincraPlayer != null) {
+//                // Combat状態にする
+//                inCombatPlayers.put(uuid, mincraPlayer);
+//                // Combat状態を解除するための処理を開始
+//                new BKTween(MincraMagics.getInstance())
+//                        .execute(v -> {
+//                            // Combat状態を解除
+//                            inCombatPlayers.remove(uuid);
+//                            return true;
+//                        })
+//                        .delay(TickTime.SECOND, 10) // 5秒後に解除
+//                        .run();
+//            }
+//        }
+//    }
 }
