b55a7e0009afac5970649c493923e316197b4b0f
Harineko0 <harineko0927@gmail.com>
2025-08-25 23:37:03 +0900

    Add React-like feature to InventoryGUI

--- src/main/java/jp/mincra/mincramagics/gui/InventoryGUI.java
+++ src/main/java/jp/mincra/mincramagics/gui/InventoryGUI.java
@@ -1,12 +1,181 @@
 package jp.mincra.mincramagics.gui;
 
+import jp.mincra.mincramagics.MincraLogger;
+import jp.mincra.mincramagics.gui.lib.GUI;
+import jp.mincra.mincramagics.gui.lib.GUIHelper;
+import jp.mincra.mincramagics.gui.lib.GuiComponent;
+import jp.mincra.mincramagics.gui.lib.State;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 
-public abstract class InventoryGUI implements Listener {
-    public InventoryGUI() {}
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
-    protected abstract void open(Player player);
+public abstract class InventoryGUI implements Listener {
+    protected Player player;
+    private final Map<Integer, List<Consumer<InventoryClickEvent>>> clickListeners = new HashMap<>();
+
+    private final List<Object> states = new ArrayList<>();
+    private final UpdateQueue updateQueue = new UpdateQueue();
+    private int lastStateIndex = -1;
+    private boolean lockRerendering = false;
+    private Predicate<Integer> isModifiableSlot = (index) -> false;
+
+    @Nullable
+    private String prevTitle = null;
+
+    public InventoryGUI() {
+    }
+
+    // API
     public abstract Inventory getInventory();
+
+    public final void open(Player player) {
+        this.player = player;
+        render();
+    }
+
+    // Rendering
+    @Nullable
+    protected abstract GUI build();
+
+    protected final void addClickListener(int index, Consumer<InventoryClickEvent> listener) {
+        if (!clickListeners.containsKey(index)) {
+            clickListeners.put(index, new ArrayList<>());
+        }
+        clickListeners.get(index).add(listener);
+    }
+
+    protected final  <T> State<T> useState(T initialValue) {
+        final int thisIndex = ++lastStateIndex;
+        final T currentState = getStateOrDefault(thisIndex, initialValue);
+        if (thisIndex < states.size()) {
+            states.set(thisIndex, currentState);
+        } else {
+            states.add(currentState);
+        }
+
+        return new State<>(currentState, (setter) -> {
+            final T newState = getStateOrDefault(thisIndex, initialValue);
+            final T updatedState = setter.apply(newState);
+            if (lockRerendering) {
+                updateQueue.add(thisIndex, updatedState);
+                return null;
+            }
+            states.set(thisIndex, updatedState);
+            // Rerender the GUI
+            render();
+            return null;
+        });
+    }
+
+    private <T> T getStateOrDefault(int index, T defaultValue) {
+        final Object thisState = index < states.size() ? states.get(index) : null;
+        final boolean isInstance = defaultValue != null && defaultValue.getClass().isInstance(thisState);
+        if (thisState != null && defaultValue != null && !isInstance) {
+            MincraLogger.warn(String.format("State at index %d is not of type %s, but %s. Using default value instead.",
+                    index, defaultValue.getClass().getName(), thisState.getClass().getName()));
+        }
+
+        @SuppressWarnings("unchecked") // We assume the type is correct
+        final T currentState = isInstance ? (T) thisState : defaultValue;
+
+        return currentState;
+    }
+
+    private void render() {
+        // FIXME: clear() よりインデックス書き換えの方がパフォーマンスが良いかもしれない
+        lastStateIndex = -1;
+        lockRerendering = true;
+        clickListeners.clear();
+
+        final GUI gui = build();
+        if (gui == null) return;
+
+        isModifiableSlot = gui.isModifiableSlot();
+        if (player.getOpenInventory().getType() != InventoryType.CHEST) {
+           player.openInventory(getInventory());
+        }
+        if (prevTitle == null || !prevTitle.equals(gui.title())) {
+            GUIHelper.updateTitle(player, gui.title());
+            prevTitle = gui.title();
+        }
+
+        for (GuiComponent component : gui.components()) {
+            Inventory inv = getInventory();
+            component.render(inv);
+
+            for (Map.Entry<Integer, List<Consumer<InventoryClickEvent>>> entry : component.getClickListeners().entrySet()) {
+                int index = entry.getKey();
+                List<Consumer<InventoryClickEvent>> listeners = entry.getValue();
+                for (Consumer<InventoryClickEvent> listener : listeners) {
+                    addClickListener(index, listener);
+                }
+            }
+        }
+
+        lockRerendering = false;
+        updateQueue.apply(item -> {
+            int index = item.index();
+            Object newValue = item.newValue();
+            if (index >= lastStateIndex) {
+                MincraLogger.warn(String.format("State index %d is out of bounds (last index: %d).", index, lastStateIndex));
+                return null; // Skip if the index is not valid
+            }
+            MincraLogger.debug("InventoryGUI: old states: " + states);
+            states.set(index, newValue);
+            MincraLogger.debug("InventoryGUI: new states: " + states);
+            // Rerender
+            render();
+            return null;
+        });
+    }
+
+    // Event handlers
+    @EventHandler // TODO: Make this method final
+    public void onClick(InventoryClickEvent event) {
+        if (!event.getInventory().equals(getInventory())) return;
+
+        event.setCancelled(!isModifiableSlot.test(event.getSlot()));
+
+        int slot = event.getSlot();
+        if (clickListeners.containsKey(slot)) {
+            for (Consumer<InventoryClickEvent> listener : clickListeners.get(slot)) {
+                listener.accept(event);
+            }
+        }
+    }
+}
+
+record UpdateQueueItem<T>(
+        int index,
+        T newValue
+) {}
+
+/**
+ * State 更新の待ちキュー
+ */
+class UpdateQueue {
+    private final List<UpdateQueueItem<?>> queue = new ArrayList<>();
+
+    public <T> void add(int index, T newValue) {
+        queue.add(new UpdateQueueItem<>(index, newValue));
+    }
+
+    public void apply(Function<UpdateQueueItem<?>, Void> updater) {
+        for (UpdateQueueItem<?> item : queue) {
+            updater.apply(item);
+        }
+        queue.clear();
+    }
 }
---
+++ src/main/java/jp/mincra/mincramagics/gui/impl/JobRewardMenu.java
@@ -0,0 +1,342 @@
+package jp.mincra.mincramagics.gui.impl;
+
+import com.gamingmesh.jobs.Jobs;
+import com.gamingmesh.jobs.container.Job;
+import io.th0rgal.oraxen.api.OraxenItems;
+import jp.mincra.mincramagics.MincraLogger;
+import jp.mincra.mincramagics.MincraMagics;
+import jp.mincra.mincramagics.config.JobRewardConfigLoader;
+import jp.mincra.mincramagics.config.model.JobRewardConfig;
+import jp.mincra.mincramagics.config.model.JobRewardsConfig;
+import jp.mincra.mincramagics.db.dao.JobRewardDao;
+import jp.mincra.mincramagics.db.model.JobReward;
+import jp.mincra.mincramagics.gui.lib.GUIHelper;
+import jp.mincra.mincramagics.gui.InventoryGUI;
+import jp.mincra.mincramagics.gui.lib.GUI;
+import jp.mincra.mincramagics.gui.lib.GuiComponent;
+import jp.mincra.mincramagics.gui.lib.GuiIcons;
+import jp.mincra.mincramagics.gui.lib.Position;
+import lombok.Builder;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class JobRewardMenu extends InventoryGUI {
+    private static final String title = GUIHelper.guiTitle("職業報酬", "%oraxen_gui_jobs_reward%");
+    private final Inventory inv;
+    private final Job job;
+    private final JobRewardsConfig jobConfig;
+    private final JobRewardDao jobRewardDao;
+
+    // Required for reflection-based instantiation
+    public JobRewardMenu() {
+        this(Jobs.getJobs().getFirst().getName());
+    }
+
+    public JobRewardMenu(String jobName) {
+        inv = Bukkit.createInventory(null, 36, Component.text(title));
+        job = Jobs.getJob(jobName);
+        jobConfig = JobRewardConfigLoader.getJobConfig(jobName);
+        jobRewardDao = MincraMagics.getJobRewardDao();
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inv;
+    }
+
+    @Override
+    protected GUI build() {
+        if (job == null) {
+            player.sendMessage(Component.text("§c◆職業が見つかりません"));
+            return null;
+        }
+        final var jobsPlayer = Jobs.getPlayerManager().getJobsPlayer(player);
+        final var progression = jobsPlayer.getJobProgression().stream().filter(
+                jobProgression -> jobProgression.getJob().equals(job)
+        ).findFirst();
+
+        if (progression.isEmpty()) {
+            player.sendMessage(Component.text(String.format("§c◆%s には加入していません", job.getName())));
+            return null;
+        }
+
+        if (jobConfig == null) {
+            player.sendMessage(Component.text(String.format("§c◆%s の報酬設定が見つかりません", job.getName())));
+            return null;
+        }
+
+        if (!jobConfig.enabled()) {
+            player.sendMessage(Component.text(String.format("§f◆%s の報酬は開発中です", job.getName())));
+            return null;
+        }
+
+        final var currentMinLevel = useState(Math.max(1, progression.get().getLevel() - 7));
+        final var jobRewards = useState(jobRewardDao.findByPlayerAndJob(player.getUniqueId(), job.getId()));
+
+        final Consumer<JobRewardConfig> handleReceiveReward = (jobRewardConfig) -> {
+            // Give the reward items to the player
+            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
+            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 2.0f);
+            if (jobRewardDao.findByPlayerAndJob(player.getUniqueId(), job.getId()).stream()
+                    .anyMatch(reward -> reward.getLevel() == jobRewardConfig.level())) {
+                player.sendMessage(Component.text("§c◆報酬はすでに受け取っています"));
+                return;
+            }
+            player.give(jobRewardConfig.items());
+
+            final var jobReward = JobReward.builder()
+                    .playerId(player.getUniqueId())
+                    .jobId(job.getId())
+                    .level(jobRewardConfig.level())
+                    .build();
+            jobRewardDao.save(jobReward);
+
+            // Update the job rewards state
+            jobRewards.set(prev -> {
+                final var updatedRewards = prev.stream()
+                        .filter(reward -> reward.getLevel() != jobRewardConfig.level())
+                        .collect(Collectors.toList());
+                updatedRewards.add(jobReward);
+                return updatedRewards;
+            });
+        };
+
+        final Consumer<Void> handleBackClick = (v) -> {
+            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
+            currentMinLevel.set((prev) -> {
+                if (prev <= 1) return 1;
+                return prev - 1;
+            });
+        };
+
+        final Consumer<Void> handleNextClick = (v) -> {
+            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
+            currentMinLevel.set((prev) -> {
+                if (prev + 8 >= job.getMaxLevel()) return prev;
+                return prev + 1;
+            });
+        };
+
+        final Consumer<Integer> handleCursorClick = (pageIndex) -> currentMinLevel.set((prev) -> {
+            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
+            MincraLogger.debug(String.format(
+                    "JobRewardMenu: Setting currentMinLevel to %d based on index %d",
+                    (int) (job.getMaxLevel() * (double) pageIndex / 7.0), pageIndex));
+            return Math.min((int) (job.getMaxLevel() * (double) pageIndex / 7.0) + 1, job.getMaxLevel() - 8);
+        });
+
+        return GUI.builder()
+                .title(title + " " + job.getDisplayName())
+                .components(List.of(
+                        new LevelDisplay(new Position(0, 0, 9), currentMinLevel.value()),
+                        new RewardDisplay(new Position(0, 1, 9), currentMinLevel.value(), jobConfig),
+                        ReceiveButtons.builder()
+                                .pos(new Position(0, 2, 9))
+                                .minLevel(currentMinLevel.value())
+                                .currentLevel(progression.get().getLevel())
+                                .rewards(jobConfig.rewards().stream()
+                                        .collect(Collectors.toMap(
+                                                JobRewardConfig::level,
+                                                JobRewardConfig::items
+                                        )))
+                                .rewardHistories(jobRewards.value().stream()
+                                        .collect(Collectors.toMap(
+                                                JobReward::getLevel,
+                                                reward -> reward
+                                        )))
+                                .onReceiveClicked(handleReceiveReward)
+                                .build(),
+                        Pagination.builder()
+                                .pos(new Position(0, 3, 9))
+                                .onBackClick(handleBackClick)
+                                .onNextClick(handleNextClick)
+                                .onPageClick(handleCursorClick)
+                                .currentPageIndex((int) (8 * ((double) currentMinLevel.value() / job.getMaxLevel())))
+                                .disableBack(currentMinLevel.value() <= 1)
+                                .disableNext(currentMinLevel.value() + 8 >= job.getMaxLevel())
+                                .build()
+                ))
+                .isModifiableSlot(s -> s >= 36)
+                .build();
+    }
+}
+
+/**
+ * レベルの数字が書いたアイコンを表示するコンポーネント
+ */
+class LevelDisplay extends GuiComponent {
+    private final Position pos;
+    private final int minLevel;
+
+    public LevelDisplay(Position pos, int minLevel) {
+        this.pos = pos;
+        this.minLevel = minLevel;
+    }
+
+    @Override
+    public void render(Inventory inv) {
+        IntStream.range(0, pos.width()).forEach(i -> inv.setItem(i + pos.startIndex(), levelIcon(minLevel + i)));
+    }
+
+    private ItemStack levelIcon(int level) {
+        return OraxenItems.getItemById("level_" + level).build();
+    }
+}
+
+/**
+ * 各レベルの報酬アイテムを表示するコンポーネント
+ */
+class RewardDisplay extends GuiComponent {
+    private final Position pos;
+    private final int minLevel;
+    private final Map<Integer, List<ItemStack>> rewards;
+
+    public RewardDisplay(Position pos, int minLevel, JobRewardsConfig config) {
+        this.pos = pos;
+        this.minLevel = minLevel;
+        this.rewards = config.rewards().stream()
+                .collect(Collectors.toMap(
+                        JobRewardConfig::level,
+                        JobRewardConfig::items
+                ));
+    }
+
+    @Override
+    public void render(Inventory inv) {
+        IntStream.range(0, pos.width()).forEach(i -> {
+            int level = minLevel + i;
+            if (rewards.containsKey(level)) {
+                final var items = rewards.get(level);
+                if (!items.isEmpty()) {
+                    inv.setItem(i + pos.startIndex(), items.getFirst()); // Set the first item of the reward
+                    return;
+                }
+            }
+
+            inv.setItem(i + pos.startIndex(), GuiIcons.invisible); // No reward for this level
+        });
+    }
+}
+
+/**
+ * それぞれの報酬が受け取られていなければ受け取りボタンを, そうでなければ受け取り済みのアイコンを表示するコンポーネント
+ */
+@Builder
+class ReceiveButtons extends GuiComponent {
+    private static final ItemStack lockedIcon = OraxenItems.getItemById("gui_job_reward_locked_icon").build();
+    private static final ItemStack receiveIcon = OraxenItems.getItemById("gui_job_reward_receive_icon").build();
+    private static final ItemStack receivedIcon = OraxenItems.getItemById("gui_job_reward_received_icon").build();
+    private final Position pos;
+    private final int minLevel;
+    private final int currentLevel;
+    private final Map<Integer, List<ItemStack>> rewards;
+    private final Map<Integer, JobReward> rewardHistories;
+    private final Consumer<JobRewardConfig> onReceiveClicked;
+
+    public ReceiveButtons(Position pos, int minLevel, int currentLevel, Map<Integer, List<ItemStack>> rewards, Map<Integer, JobReward> rewardHistories, Consumer<JobRewardConfig> onReceiveClicked) {
+        this.pos = pos;
+        this.minLevel = minLevel;
+        this.currentLevel = currentLevel;
+        this.rewards = rewards;
+        this.rewardHistories = rewardHistories;
+        this.onReceiveClicked = onReceiveClicked;
+    }
+
+    @Override
+    public void render(Inventory inv) {
+        IntStream.range(0, pos.width()).forEach(i -> {
+            final var level = minLevel + i;
+            final var reward = rewards.get(level);
+            if (reward == null || reward.isEmpty()) {
+                inv.setItem(i + pos.startIndex(), GuiIcons.invisible); // No reward for this level
+                return;
+            }
+            final var rewardHistory = rewardHistories.get(level);
+            if (rewardHistory != null) {
+                inv.setItem(i + pos.startIndex(), receivedIcon); // Set received icon
+                return;
+            }
+
+            if (level > currentLevel) {
+                inv.setItem(i + pos.startIndex(), lockedIcon); // Set locked icon
+                return;
+            }
+
+            inv.setItem(i + pos.startIndex(), receiveIcon);
+            addClickListener(i + pos.startIndex(), e -> {
+                e.setCancelled(true);
+                onReceiveClicked.accept(new JobRewardConfig(level, rewards.get(level)));
+            });
+        });
+    }
+}
+
+@Builder
+class Pagination extends GuiComponent {
+    private final Position pos;
+    private final Consumer<Void> onBackClick;
+    private final Consumer<Void> onNextClick;
+    private final Consumer<Integer> onPageClick;
+    private final int currentPageIndex;
+    private final boolean disableBack;
+    private final boolean disableNext;
+
+    public Pagination(Position pos, Consumer<Void> onBackClicked, Consumer<Void> onNextClicked, Consumer<Integer> onPageClick, int currentPageIndex, boolean disableBack, boolean disableNext) {
+        this.pos = pos;
+        this.onBackClick = onBackClicked;
+        this.onNextClick = onNextClicked;
+        this.onPageClick = onPageClick;
+        this.currentPageIndex = currentPageIndex >= pos.width() ? pos.width() - 1 : currentPageIndex;
+        this.disableBack = disableBack;
+        this.disableNext = disableNext;
+
+        if (currentPageIndex >= pos.width()) {
+            MincraLogger.warn(String.format(
+                    "Pagination currentPageIndex %d is greater than width %d. Resetting to last index.",
+                    currentPageIndex, pos.width()));
+        }
+    }
+
+    @Override
+    public void render(Inventory inv) {
+        inv.setItem(pos.startIndex(), disableBack ? GuiIcons.leftArrowDisabled : GuiIcons.leftArrow);
+        IntStream.range(0, pos.width() - 2)
+                .forEach(i -> {
+                    final var index = i + pos.startIndex() + 1;
+                    if (i == currentPageIndex) {
+                        inv.setItem(index, GuiIcons.horizontalCursor);
+                        return;
+                    }
+                    inv.setItem(index, GuiIcons.invisible);
+                    addClickListener(index, e -> {
+                        e.setCancelled(true);
+                        onPageClick.accept(i);
+                    });
+                }); // Fill empty slots with invisible items
+        inv.setItem(pos.endIndex(), disableNext ? GuiIcons.rightArrowDisabled : GuiIcons.rightArrow);
+
+        // Add click listeners for back and next arrows
+        if (!disableBack) {
+            addClickListener(pos.startIndex(), e -> {
+                e.setCancelled(true);
+                onBackClick.accept(null);
+            });
+        }
+
+        if (!disableNext) {
+            addClickListener(pos.endIndex(), e -> {
+                e.setCancelled(true);
+                onNextClick.accept(null);
+            });
+        }
+    }
+}
