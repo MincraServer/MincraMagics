c11c6ae5f36520accf57c88e73eae1f10eac0e15
Harineko0 <harineko0927@gmail.com>
2025-08-28 02:21:44 +0900

    GUIHelper.guiTitle がずれるバグ修正

--- src/main/java/jp/mincra/mincramagics/gui/lib/GUIHelper.java
+++ src/main/java/jp/mincra/mincramagics/gui/lib/GUIHelper.java
@@ -26,15 +26,38 @@ public class GUIHelper {
                 .run();
     }
 
-    public static String guiTitle(String name, String glyphPlaceholder) {
+    /**
+     * @param name             The name of the GUI.
+     * @param glyphPlaceholder The placeholder for the glyph to be used as an icon.
+     * @param height           The height of the GUI in rows (1-6).
+     * @return The formatted title string.
+     */
+    public static String guiTitle(String name, String glyphPlaceholder, int height) {
         return Color.COLOR_WHITE +
                 PlaceholderAPI.setPlaceholders(null, Glyphs.NEG_8 + glyphPlaceholder)
                 + Color.COLOR_DARK_GRAY
-                + PlaceholderAPI.setPlaceholders(null, Glyphs.NEG_10 + Glyphs.NEG_32 + Glyphs.NEG_128 + name);
+                // if height is
+                // - 1: NEG_244 in total
+                // - 2: NEG_220 in total
+                // - 3: NEG_196 in total
+                // - 4: NEG_170 in total
+                // - 5: NEG_144 in total
+                // - 6: NEG_128 in total
+                + PlaceholderAPI.setPlaceholders(null, switch (height) {
+                    // FIXME: Use a loop to generate the string instead of hardcoding it
+            case 1 -> Glyphs.NEG_128 + Glyphs.NEG_64 + Glyphs.NEG_32 + Glyphs.NEG_16 + Glyphs.NEG_4;
+            case 2 -> Glyphs.NEG_128 + Glyphs.NEG_64 + Glyphs.NEG_16 + Glyphs.NEG_8 + Glyphs.NEG_4;
+            case 3 -> Glyphs.NEG_128 + Glyphs.NEG_64 + Glyphs.NEG_4;
+            case 4 -> Glyphs.NEG_128 + Glyphs.NEG_32 + Glyphs.NEG_8 + Glyphs.NEG_2;
+            case 5 -> Glyphs.NEG_128 + Glyphs.NEG_16 + Glyphs.NEG_4;
+            case 6 -> Glyphs.NEG_128 + Glyphs.NEG_8;
+            default -> Glyphs.NEG_128 + Glyphs.NEG_64;
+        } + name);
     }
 
     /**
      * Get the lowest empty slot in the inventory.
+     *
      * @param inv The inventory to check.
      * @return The index of the lowest empty slot, or -1 if the inventory is full.
      */
