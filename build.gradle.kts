c6ea5761b80b24b5127f71e2c8b058fb4192fb2b
Harineko0 <harineko0927@gmail.com>
2025-08-30 19:48:44 +0900

    Update dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,11 +69,12 @@ dependencies {
     implementation("de.tr7zw:item-nbt-api-plugin:2.12.2")
     compileOnly("io.lumine:Mythic-Dist:5.3.5")
     compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
-    compileOnly("dev.jorel:commandapi-bukkit-core:9.3.0")
+    compileOnly("dev.jorel:commandapi-bukkit-core:10.1.1")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
     compileOnly(files("libs/Jobs5.2.6.3.jar")) // Jobs Reborn
     compileOnly(files("libs/oraxen-1.190.0-modified.jar")) // // 変更を加えた Oraxen を libs フォルダに配置
+//    compileOnly(files("libs/InfiniteCrops-1.4.8.jar"))
     compileOnly(files("libs/InfiniteFishing-1.6.1.jar"))
     compileOnly(files("libs/GSit-2.4.2.jar"))
     // compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar")))) // NMS
@@ -83,7 +84,7 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:3.50.3.0")
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
-    compileOnly("net.kyori:adventure-platform-bukkit:4.3.4")
+    compileOnly("net.kyori:adventure-platform-bukkit:4.4.1")
     // For jp.mincra.ezsvg
     implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
     implementation("org.w3c:dom:2.3.0-jaxb-1.0.6")
